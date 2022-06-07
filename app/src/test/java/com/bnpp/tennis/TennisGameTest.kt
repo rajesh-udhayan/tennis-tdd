1dc4dde27ceed34d957fd8528c275f9eaf44a116
rajesh-udhayan <rajeshkudhayan@gmail.com>
2022-06-07 16:02:00 +0530

    Refactor Generate score method added for readability

--- app/src/test/java/com/bnpp/tennis/TennisGameTest.kt
+++ app/src/test/java/com/bnpp/tennis/TennisGameTest.kt
@@ -34,59 +34,52 @@ class TennisGameTest {
 
     @Test
     fun `should return 15 , 15 when both player scores`(){
-        game.playerOneScores()
-        game.playerTwoScores()
+        generateScore(1,1)
 
         assertThat(game.getScore()).isEqualTo("15|15")
     }
 
     @Test
     fun `should return 30, 0 when player one scores two times`() {
-        game.playerOneScores()
-        game.playerOneScores()
+        generateScore(2)
 
         assertThat(game.getScore()).isEqualTo("30|0")
     }
 
     @Test
     fun `should return 0 , 40 when player two scores three times`(){
-        game.playerTwoScores()
-        game.playerTwoScores()
-        game.playerTwoScores()
+        generateScore(playerTwoPoints = 3)
 
         assertThat(game.getScore()).isEqualTo("0|40")
     }
 
     @Test
     fun `should return Player1 wins when player one scores more than 40`(){
-        game.playerOneScores()
-        game.playerOneScores()
-        game.playerOneScores()
-        game.playerOneScores()
+        generateScore(4)
 
         assertThat(game.getScore()).isEqualTo("Player1 wins")
     }
 
     @Test
     fun `should return Player2 wins when player two scores more than 40`(){
-        game.playerTwoScores()
-        game.playerTwoScores()
-        game.playerTwoScores()
-        game.playerTwoScores()
+        generateScore(playerTwoPoints = 4)
 
         assertThat(game.getScore()).isEqualTo("Player2 wins")
     }
 
     @Test
     fun `should return deuce when both players are 40 or more than 40`(){
-        for (i in 1..4){
-            game.playerOneScores()
-        }
-
-        for (i in 1..4){
-            game.playerTwoScores()
-        }
+        generateScore(4,4)
 
         assertThat(game.getScore()).isEqualTo("Deuce")
+    }
+
+    fun generateScore(playerOnePoints: Int = 0, playerTwoPoints: Int = 0){
+        for (i in 1..playerOnePoints){
+            game.playerOneScores()
+        }
+        for (i in 1..playerTwoPoints){
+            game.playerTwoScores()
+        }
     }
 }
