c88079ff63561975c607257f9798213311a5fcdf
rajesh-udhayan <rajeshkudhayan@gmail.com>
2022-06-07 14:44:36 +0530

    Refactor Added tennis initialization to setup method

--- app/src/test/java/com/bnpp/tennis/TennisGameTest.kt
+++ app/src/test/java/com/bnpp/tennis/TennisGameTest.kt
@@ -1,21 +1,25 @@
 package com.bnpp.tennis
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Test
 
 class TennisGameTest {
 
+    private lateinit var game: TennisGame
+
+    @Before
+    fun setUp(){
+         game = TennisGame()
+    }
+
     @Test
     fun `should return 0 , 0 as score when new game started`(){
-        val game = TennisGame()
-
         assertThat(game.getScore()).isEqualTo("0|0")
     }
 
     @Test
     fun `should return 1 , 0 when player one scores`(){
-        val game = TennisGame()
-
         game.playerOneScores()
 
         assertThat(game.getScore()).isEqualTo("1|0")
